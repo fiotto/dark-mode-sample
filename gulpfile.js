100c2c6218bb5906bb9480dee333eee195ccc540
fiotto <fiotto42@gmail.com>
2020-12-05 21:48:17 +0900

    add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const connect = require('gulp-connect');
+const mode = require('gulp-mode')({
+    modes: ['production', 'development'],
+    default: 'development',
+    verbose: false
+});
+
+exports.default = function (cb) {
+    console.log('gulp run');
+    cb();
+}
+
+function html() {
+    return gulp.src('src/html/index.pug')
+        .pipe(pug())
+        .pipe(rename({ exports: '.html' }))
+        .pipe(gulp.dest('./dist'));
+}
+
+function style() {
+    return gulp.src('src/sass/style.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(mode.development(sourcemaps.write('.')))
+        .pipe(gulp.dest('./dist'));
+}
+
+function server() {
+    return connect.server({
+        root: 'dist',
+        host: '0.0.0.0',
+        livereload: true
+    })
+}
+
+const build = gulp.parallel(html, style);
+
+const watch = gulp.parallel(build, function(cb) {
+    gulp.watch('src/html/**/*.pug', html);
+    gulp.watch('src/sass/**/*.sass', style);
+    cb();
+})
+
+const dev = gulp.parallel(server, watch)
+
+exports.html = html;
+exports.style = style;
+exports.build = build;
+exports.server = server;
+exports.watch = watch;
+exports.dev = dev;
